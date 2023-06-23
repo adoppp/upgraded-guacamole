1bf427d617024a5e344536acdc29984c0d778a5f
adoppp <121878757+adoppp@users.noreply.github.com>
2023-06-23 17:16:13 +0200

    sosi

--- js/script.js
+++ js/script.js
@@ -7,6 +7,15 @@ buttonClick.addEventListener('click', () => {
     if (name === '') {
         alert('Ты не ввел имя, уебище!')
     }
+    else if (name === null) {
+        alert('Не наебешь.');
+
+        let pTrue = document.createElement('p');
+        pTrue.textContent = `У меня камера лучше соси`;
+        pTrue.style.fontSize = '60px';
+
+        return divBlock.insertAdjacentElement('beforebegin', pTrue);
+    }
     else {
         let pTrue = document.createElement('p');
         pTrue.textContent = `Добро пожаловать в гейский клуб голубая устрица) ${name} )`;
